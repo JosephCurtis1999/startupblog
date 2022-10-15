604c5b80305b4bf9306f69a1d874e7e87728e336
Joseph Curtis <joecurtis9999@gmail.com>
2022-10-15 10:57:11 +0100

    jsx home page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from '../components';
 import Image from 'next/image'
 
 const posts = [
@@ -16,17 +17,13 @@ const Home: NextPage = () => {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-        {posts.map((post, index) => (
-        <div>
-          {post.title}
-          {post.excerpt}
-          </div>
-          ))}
+        {posts.map((post, index) => <PostCard post ={post} key={post.title}/>)}
 
       </div>
       <div className='lg:col-span-4 col-span-1'>
         <div className='lg:sticky relative top-8'>
-          
+          <PostWidget />
+          <Categories />
         </div>
       </div>
 
