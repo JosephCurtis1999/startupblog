2c56d5a2378bf722081ea24818502a0963b1dbf9
Joseph Curtis <joecurtis9999@gmail.com>
2022-10-16 15:23:36 +0100

    fetching fixes

--- components/Postcard.jsx
+++ components/Postcard.jsx
@@ -20,7 +20,21 @@ const Postcard = ({ post }) => {
       <div className='bloc lg:flex text-center items-center justify-center mb-8 w-full'>
         <div className='flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8'>
           <img alt={post.author.name} height="30px" width="30px" className='align-middle rounded-full' src={post.author.photo.url} />
+          <p className='inline align-middle text-black-600 ml-2 text-lg'></p>
         </div>
+        <div className='font-medium text-black-600'>
+          <span>
+            {moment(post.createdAt).format('MMM DD, YYYY')}
+          </span>
+        </div>
+      </div>
+      <p className='text-center text-lg text-black-600 font-normal px-4 lg:px-20 mb-8'>{post.excerpt}</p>
+      <div className='text-center'>
+        <Link href={`/post/${post.slug}`}>
+          <span className='transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer'>
+            Continue Reading
+          </span>
+        </Link>
       </div>
     </div>
   )
