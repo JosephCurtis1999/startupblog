847b345b56afe707a6ae85004ff2c368296d36af
Joseph Curtis <joecurtis9999@gmail.com>
2022-10-16 11:42:23 +0100

    adding post query to fetch data from CMS

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,18 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import Head from 'next/head';
 import { PostCard, Categories, PostWidget } from '../components';
-import Image from 'next/image'
+import { getPosts } from '../services'
 
-const posts = [
-  { title: 'React Testing', excerpt: 'Learn React Testing' },
-  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' }
-];
 
-const Home: NextPage = () => {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>Startup Blog</title>
+        <title>Scoop</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-        {posts.map((post, index) => <PostCard post ={post} key={post.title}/>)}
+        {posts.map((post) => <PostCard post ={post} key={post.title}/>)}
 
       </div>
       <div className='lg:col-span-4 col-span-1'>
@@ -32,4 +27,11 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export async function getStaticProps() {
+  const posts = (await getPosts() ) || [];
+
+  return {
+    props: { posts }
+  }
+}
+
---
+++ services/index.js
@@ -0,0 +1,40 @@
+import { request, gql } from 'graphql-request';
+
+const graphqlAPI = process.env.NEXT_PUBLIC_SCOOP_ENDPOINT;
+
+export const getPosts = async () => {
+    const query = gql`
+    query MyQuery {
+        postsConnection {
+          edges {
+            node {
+              author {
+                bio
+                name
+                id
+                photo {
+                  fileName
+                }
+              }
+              createdAt
+              slug
+              title
+              excerpt
+              featuredImage {
+                fileName
+              }
+              categories {
+                name
+                slug
+              }
+            }
+          }
+        }
+      }
+    `;
+
+    const result = await request (graphqlAPI, query);
+
+    return result.postsConnection.edges;
+}
+
