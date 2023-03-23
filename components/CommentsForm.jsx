ec1bf3575e0b3a84e779bee4125bed4b7949f862
Joseph Curtis <joecurtis9999@gmail.com>
2023-03-23 16:54:03 +0000

    building out the comments form component

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 
-const CommentsForm = () => {
+const CommentsForm = ({ slug }) => {
+  const [error, setError] = useState(false);
+  const [localStorage, setLocalStorage] = useState(null);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const commentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDataEl = useRef();
+
+
+
+
   return (
-    <div>
-        <h1>CommentsForm</h1>
+    <div className='bg-black bg-opacity-20 text-white rounded-lg p-8 pb-12 mb-8'>
+        <h3 className='text-xl mb-8 font-semibold border-b pb-4'>Comments Form</h3>
+        <div className='grid grid-cols-1 gap-4 mb-4'>
+          <textarea 
+          ref={commentEl} 
+          className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-white text-gray-700"
+
+          />
+
+        </div>
+        <div className='grid grid-cols-1 gap-4 mb-4'>
+
+        </div>
+        <div className='grid grid-cols-1 gap-4 mb-4'>
+
+        </div>
     </div>
   )
 }
